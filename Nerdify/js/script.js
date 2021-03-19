ed75376814f442318beff1ff9da65f7240db8d2d
Tochynskii <nsassault@gmail.com>
2021-03-19 13:55:14 +0200

    Nerdify site

--- Nerdify/js/script.js
+++ Nerdify/js/script.js
@@ -1,3 +1,37 @@
 $(document).ready(function () {
-	$('.slider').slick()
+	$('.slider').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 1,
+		autoplay: true,
+		infinite: true,
+		speed: 1000,
+		dots: true,
+		initialSlide: 0,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 769,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+		]
+	})
 });
+
